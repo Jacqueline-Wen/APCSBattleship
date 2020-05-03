9c908b3f3b93a5cb9b53677f0b7969caf464ae9e
Megha <Megha@DESKTOP-KQ6NATG>
2020-05-03 14:41:42 -0700

    meghana's test commit

---
+++ APCSfinalproject/test.java
@@ -0,0 +1,57 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+public class test
+{
+
+    private JFrame frame;
+
+    /**
+     * Launch the application.
+     */
+    public static void main( String[] args )
+    {
+        EventQueue.invokeLater( new Runnable()
+        {
+            public void run()
+            {
+                try
+                {
+                    test window = new test();
+                    window.frame.setVisible( true );
+                }
+                catch ( Exception e )
+                {
+                    e.printStackTrace();
+                }
+            }
+        } );
+    }
+
+
+    /**
+     * Create the application.
+     */
+    public test()
+    {
+        initialize();
+    }
+
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize()
+    {
+        frame = new JFrame();
+        frame.setBounds( 100, 100, 450, 300 );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.getContentPane().setLayout(null);
+        
+        JButton btnNewButton = new JButton("A1");
+        btnNewButton.setBounds(15, 16, 115, 37);
+        frame.getContentPane().add(btnNewButton);
+    }
+}

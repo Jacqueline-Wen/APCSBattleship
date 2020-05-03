ee4dd38235b01ca7a830bc5268aa44309fa31073
Jacqueline Wen <jackierwen@gmail.com>
2020-05-03 16:14:37 -0700

    add turn boolean

--- Battleship/PersonVsPerson.java
+++ Battleship/PersonVsPerson.java
@@ -2,6 +2,7 @@ public class PersonVsPerson
 {
     private boolean [][] person1;
     private boolean [][] person2;
+    private boolean turn;
     public PersonVsPerson() {
         person1 = new boolean[10][10];
         person2 = new boolean[10][10];

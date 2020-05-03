f3f2bf2bff7bdc3cf58b946693682b6d9fca0bb1
Jacqueline Wen <jackierwen@gmail.com>
2020-05-02 21:21:25 -0700

    Created 2 classes and main

---
+++ Battleship/PersonVsComputer.java
@@ -0,0 +1,16 @@
+
+public class PersonVsComputer
+{
+    private boolean [][] person;
+    private boolean [][] computer;
+    public PersonVsComputer() {
+        person = new boolean[10][10];
+        computer = new boolean[10][10];
+        choose();
+    }
+    
+    public void choose() {
+        System.out.println("Person1, please choose ur location");
+        
+    }
+}

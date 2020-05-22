a341b1e9bcd143888c65f5ccba838f32fc60da45
Jacqueline Wen <jackierwen@gmail.com>
2020-05-22 07:08:33 -0700

    Update PersonVsPerson.java

--- Battleship/PersonVsPerson.java
+++ Battleship/PersonVsPerson.java
@@ -35,7 +35,18 @@ public class PersonVsPerson
         person = !person;
         while (gameOver() == 0) {
             guessMove(person);
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if (person) {
+                        System.out.print( person1map[i][j] + " " );
+                    } else {
+                        System.out.print( person2map[i][j] + " ");
+                    }
+                }
+                System.out.println();
+            }
             person = !person;
+            System.out.println("Sorry, this coordinate did not hit a battleship");
             for (int i = 0; i < 15; i++) {
                 System.out.println();
             }
@@ -72,6 +83,14 @@ public class PersonVsPerson
         int rval, cval;
         rval = in.nextInt();
         cval = in.nextInt();
+        while (rval < 0 || rval > 9) {
+            System.out.println("Please inset another value for guessed row number");
+            rval = in.nextInt();
+        }
+        while (cval < 0 || cval > 9) {
+            System.out.println("Please inset another value for guessed column number");
+            cval = in.nextInt();
+        }
         int curPos = 0;
         if (person) {
             curPos = person1[rval][cval];
@@ -98,39 +117,11 @@ public class PersonVsPerson
                 person2[rval][cval] = 3;
                 person2map[rval][cval] = 'X';
             }
-//            for (int i = 0; i < 10; i++) {
-//                for (int j = 0; j < 10; j++) {
-//                    if (person) {
-//                        System.out.print( person1map[i][j] + " " );
-//                    } else {
-//                        System.out.print( person2map[i][j] + " ");
-//                    }
-//                }
-//                System.out.println();
-//            }
-//            
-//            System.out.println();
             if (gameOver() == 1 || gameOver() == 2) {
                 return;
             } 
             guessMove(person);
         }
-        
-//        if (gameOver() == 0) {
-//            for (int i = 0; i < 10; i++) {
-//                for (int j = 0; j < 10; j++) {
-//                    if (person) {
-//                        System.out.print( person1map[i][j] + " ");
-//                    } else {
-//                        System.out.print( person2map[i][j] + " ");
-//                    }
-//                }
-//                System.out.println();
-//            }
-//            System.out.println();
-//        }
-        
-        
     }
     
     /**
@@ -162,13 +153,37 @@ public class PersonVsPerson
         return 0;
     }
     
+    private boolean positionWorks(int rval, int cval, int direction, int length) {
+        int curr = rval;
+        int curc = cval;
+        for (int i = 0; i < length; i++) {
+            if (person) {
+                if (curr > 9 || curc > 9 || person2[curr][curc] == 1) {
+                    return false;
+                } 
+            }else {
+                if (curr > 9 || curc > 9 || person1[curr][curc] == 1) {
+                    return false;
+                }
+            }
+            
+            if (direction == 0) {
+                curc++;
+            } else {
+                curr++;
+            }
+        }
+        return true;
+    }
+    
+    
     //1 boat size 5, 2 boat size 4, 3 boat size 3, 4 boat size 2
     
     private void drawMap(int rval, int cval, int length, int direction) {
         //horizontally
         if (direction == 0) {
             for (int i = cval; i < cval + length; i++) {
-                if (person) {
+                if (!person) {
                     person1[rval][i] = 1;
                 } else {
                     person2[rval][i] = 1;
@@ -179,7 +194,7 @@ public class PersonVsPerson
         //vertically
         if (direction == 1) {
             for (int i = rval; i < rval + length; i++) {
-                if (person) {
+                if (!person) {
                     person1[i][cval] = 1;
                 } else {
                     person2[i][cval] = 1;
@@ -189,7 +204,7 @@ public class PersonVsPerson
         
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                if (person) {
+                if (!person) {
                     System.out.print( person1[i][j] + " " );
                 } else {
                     System.out.print( person2[i][j] + " ");
@@ -207,23 +222,96 @@ public class PersonVsPerson
         a = in.nextInt();
         b = in.nextInt();
         c = in.nextInt();
+        while (a < 0 || a > 9) {
+            System.out.println("Please inset another value for row number");
+            a = in.nextInt();
+        }
+        while (b < 0 || b > 9) {
+            System.out.println("Please inset another value for column number");
+            b = in.nextInt();
+        }
+        while (c < 0 || c > 1) {
+            System.out.println("Please inset another value for direction (0 - horizontal, 1-vertical)");
+            c = in.nextInt();
+        }
+        while (!positionWorks(a, b, c, 5)) {
+            System.out.println("There was an overlap on the map, please insert new values for row, column and direction");
+            a = in.nextInt();
+            b = in.nextInt();
+            c = in.nextInt();
+        }
         drawMap(a, b, 5, c);
         for (int i = 0; i < 2; i++) {
             a = in.nextInt();
             b = in.nextInt();
             c = in.nextInt();
+            
+            while (a < 0 || a > 9) {
+                System.out.println("Please inset another value for row number");
+                a = in.nextInt();
+            }
+            while (b < 0 || b > 9) {
+                System.out.println("Please inset another value for column number");
+                b = in.nextInt();
+            }
+            while (c < 0 || c > 1) {
+                System.out.println("Please inset another value for direction (0 - horizontal, 1-vertical)");
+                c = in.nextInt();
+            }
+            while (!positionWorks(a, b, c, 4)) {
+                System.out.println("There was an overlap on the map, please insert new values for row, column and direction");
+                a = in.nextInt();
+                b = in.nextInt();
+                c = in.nextInt();
+            }
             drawMap(a, b, 4, c);
         }
         for (int i = 0; i < 3; i++) {
             a = in.nextInt();
             b = in.nextInt();
             c = in.nextInt();
+            while (a < 0 || a > 9) {
+                System.out.println("Please inset another value for row number");
+                a = in.nextInt();
+            }
+            while (b < 0 || b > 9) {
+                System.out.println("Please inset another value for column number");
+                b = in.nextInt();
+            }
+            while (c < 0 || c > 1) {
+                System.out.println("Please inset another value for direction (0 - horizontal, 1-vertical)");
+                c = in.nextInt();
+            }
+            while (!positionWorks(a, b, c, 3)) {
+                System.out.println("There was an overlap on the map, please insert new values for row, column and direction");
+                a = in.nextInt();
+                b = in.nextInt();
+                c = in.nextInt();
+            }
             drawMap(a, b, 3, c);
         }
         for (int i = 0; i < 4; i++) {
             a = in.nextInt();
             b = in.nextInt();
             c = in.nextInt();
+            while (a < 0 || a > 9) {
+                System.out.println("Please inset another value for row number");
+                a = in.nextInt();
+            }
+            while (b < 0 || b > 9) {
+                System.out.println("Please inset another value for column number");
+                b = in.nextInt();
+            }
+            while (c < 0 || c > 1) {
+                System.out.println("Please inset another value for direction (0 - horizontal, 1-vertical)");
+                c = in.nextInt();
+            }
+            while (!positionWorks(a, b, c, 2)) {
+                System.out.println("There was an overlap on the map, please insert new values for row, column and direction");
+                a = in.nextInt();
+                b = in.nextInt();
+                c = in.nextInt();
+            }
             drawMap(a, b, 2, c);
         }
     }
